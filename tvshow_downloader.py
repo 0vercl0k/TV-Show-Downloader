d6a0287cee4f0c462c389c0ef7bac52bd9761a5d
Axel "0vercl0k" Souchet <0vercl0k@tuxfamily.org>
2012-03-01 22:26:52 +0100

    * Add comments * Clean a bit Episode class => use if/else compound statement

--- tvshow_downloader.py
+++ tvshow_downloader.py
@@ -31,12 +31,27 @@ import os
 import ConfigParser
 
 class TVShowConfigurationParser:
+	'''
+	This class rules the configuration file.
+	If the file doesn't exist, it'll create it.
+
+	Here is the an example configuration file:
+	[global]
+	magnet_file = bla
+	log_file = bla
+
+	[seriename]
+	hd = True
+
+	The global section is required to launch the script.
+	'''
 	def __init__(self, conf_file = './tvshow_downloader.cfg'):
 		self.global_conf = {}
-		self.series = []
+		self.series      = []
 
 		parser = ConfigParser.ConfigParser()
 		
+		# we create the file if it doesn't exist: too kind yeah.
 		if len(parser.read(conf_file)) == 0:
 			parser.add_section('global')
 			parser.set('global', 'magnet_file', '/tmp/magnetz')
@@ -44,14 +59,15 @@ class TVShowConfigurationParser:
 			parser.write(open('./tvshow_downloader.cfg', 'w'))
 
 		try:
-			# now we check the datas
+			# let me extract the required fields
 			self.global_conf['magnet_file'] = parser.get('global', 'magnet_file')
 			self.global_conf['log_file'] = parser.get('global', 'log_file')
 
+			# maybe you haven't add yet your serie
 			if len(parser.sections()) == 1:
 				return
 
-			# now the series
+			# each others sections are reserved to describe a serie
 			for section in parser.sections():
 				if section != 'global':
 					self.series.append({
@@ -71,15 +87,20 @@ class TVShowConfigurationParser:
 		return self.series
 
 class Episode:
-	"""class describing an episode"""
+	'''
+	Extract some useful information about an episode:
+		* Is it an HD one ?
+		* In which seasons the episode is ?
+		* What is the episode number ?
+	
+	BTW, the eztv episodes are always named according to this way:
+		seriename [0-9]{1,2}x[0-9]{1,2} [ quality ] -- Spartacus 2x5 [HDTV - ASAP]
+	'''
 	def __init__(self, name):
 		self.name = name
 		self.info = {}
 
-		if name.lower().find('720p') != -1:
-			self.info['is_hd'] = 1
-		else:
-			self.info['is_hd'] = 0
+		self.info['is_hd'] = 1 if name.lower().find('720p') != -1 else 0
 
 		r = re.findall(name, '([0-9]{1,2})x([0-9]{1,2})')
 		if len(r) == 0:
@@ -102,6 +123,10 @@ class Episode:
 		return self.info['is_hd']
 
 class DownloadHistory:
+	'''
+	A little class to keep a writing trace of the differents downloads,
+	it can be cool to see in your bash the latest downloaded files.
+	'''
 	def __init__(self, log_file):
 		try:
 			self.log = open(log_file, 'a')
@@ -116,6 +141,7 @@ class DownloadHistory:
 		if self.header_put == False:
 			self.__write_header()
 			self.header_put = True
+
 		self.log.write('Downloaded : %s\n' % name)
 
 	def __del__(self):
@@ -125,17 +151,25 @@ class DownloadHistory:
 			pass
 
 class TVShows_Manager:
-	"""This class manages your favorite TV Shows download"""
+	'''
+	THE TVShows_Manager !
+
+	It checks the latest episode of a serie:
+		-> if this one is in the database, ok you already see it
+		-> else, I write the magnet link in a specific file -- like that you can add it in your torrent manager
+
+	The database is a very basic sqlite3 base, with a table for each serie
+	'''
 	def __init__(self, favorite_tv_show, log_file, magnets_file):
-		self.fav = favorite_tv_show
-		self.co = sqlite3.connect('./tvshow_downloader.db')
-		self.c = self.co.cursor()
-		self.logger = DownloadHistory(log_file)
+		self.fav          = favorite_tv_show
+		self.co           = sqlite3.connect('./tvshow_downloader.db')
+		self.c            = self.co.cursor()
+		self.logger       = DownloadHistory(log_file)
 		self.magnets_file = magnets_file
 
 		# initialize the tables
 		for show in self.fav:
-			# Create the table if it doesn't exist yet
+			# create the table if it doesn't exist yet
 			self.c.execute('''
 				CREATE TABLE 
 				IF NOT EXISTS "%s" 
@@ -150,17 +184,25 @@ class TVShows_Manager:
 			self.co.commit()
 
 	def __parse_eztv(self, tv_show):
+		'''
+		Parse the eztv website, and raise an exception if something goes wrong
+		'''
 		params = urlencode({
 			'show_name' : tv_show,
 			'mode' : 'rss'
 		})
 
 		f = parse('http://www.ezrss.it/search/index.php?' + params)
-		if f.bozo == 1:
+		if f.bozo == 1: # BTW why the exception flag is called 'bozo' ? #wtf
 			raise f.bozo_exception
+
+		# we want only the latest entry -- because I assume you launch the script at least one time each day
 		return f.entries[0]
 
 	def __is_episode_already_downloaded(self, tv_show, ep_name):
+		'''
+		Check if an episode is already in the database
+		'''
 		self.c.execute('SELECT count(*) FROM "%s" WHERE name = ?' % tv_show, (ep_name, ))
 		row = self.c.fetchone()
 		if row[0] == 0:
@@ -168,6 +210,9 @@ class TVShows_Manager:
 		return True
 
 	def __get_last_episode(self, tv_show, hd = False):
+		'''
+		Retrives the latest un-downloaded episode of a specific tv show
+		'''
 		try:
 			last_ep = self.__parse_eztv(tv_show)
 			is_already_downloaded = self.__is_episode_already_downloaded(tv_show, last_ep.title)
@@ -178,11 +223,14 @@ class TVShows_Manager:
 			print 'An error occured buddy: ' + str(e)
 
 	def checkout(self):
+		'''
+		Find the latest episode for each your serie and write their magnet URI into a specific file
+		'''
 		file_magnets = open(self.magnets_file, 'w')
 		nb_files_down = 0
 
 		for show in self.fav:
-			# trying to retrieve the last episode
+			# trying to retrieve the latest episode
 			last_ep = self.__get_last_episode(show['name'])
 			if last_ep != None:
 
@@ -203,10 +251,11 @@ class TVShows_Manager:
 					episode.is_an_hd_episode(),
 					time.time()
 				)
-
+				# OK, now I assume you'll start the torrent soon ; we don't want to re-download this file again
 				self.c.execute('INSERT INTO "%s" VALUES(NULL, ?, ?, ?, ?, ?)' % show['name'], attrs)
 				self.co.commit()
 				nb_files_down += 1
+
 		file_magnets.close()
 		return nb_files_down
 
@@ -214,31 +263,36 @@ class TVShows_Manager:
 		self.co.close()
 
 def main(argc, argv):
-	# I RECOMMEND YOU:
-	# 1] create a little bash script to start this script -- see run_tvshow_downloader.sh
+	'''
+	I RECOMMEND YOU:
+	1] create a little bash script to start this script -- see run_tvshow_downloader.sh
+	
+	2] add a crontab to launch the whole process when you want
+		crontab -e
+		0 * * * * run_tvshow_downloader.sh
+	
+	3] add to your ~/.bashrc something like:
+		echo "HERE ARE THE LATEST DOWNLOADS:"
+		tail -20 ~/.logz_dl
 
-	# 2] add a crontab to launch the whole process as you want
-	# crontab -e
-	# 0 * * * * run_tvshow_downloader.sh
+	Don't forgot to check if you serie exists there:
+		http://www.ezrss.it/shows/ (and verify eztv team releases 720p files for your series)
+	'''
 
-	# 3] add to your ~/.bashrc something like:
-	# echo "HERE ARE THE LAST DOWNLOADS:"
-	# tail -20 ~/.logz_dl
-
-	# I assume all your series exist there:
-	#	http://www.ezrss.it/shows/ (and verify eztv team releases 720p files for your series)
 	try:
+		# try to parse the configuration file
 		conf_manager = TVShowConfigurationParser()
 	except Exception, e:
 		print 'Your configuration file seems to sucks, here is the exception:'
 		print str(e)
 
+	# OK, get ready to checkout!
 	shows_manager = TVShows_Manager(
 		conf_manager.get_series(),
 		conf_manager.get_log_file(),
 		conf_manager.get_magnet_file()
 	)
-	
+
 	print '%d magnets added.' % shows_manager.checkout()
 	return 1
 
