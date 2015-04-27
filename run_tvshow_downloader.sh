97e490364910d7858ec89e7ff4ecd47ebdfe124c
Axel "0vercl0k" Souchet <0vercl0k@tuxfamily.org>
2015-04-26 22:50:25 -0700

    specify host

--- run_tvshow_downloader.sh
+++ run_tvshow_downloader.sh
@@ -9,7 +9,7 @@ if [ -f $MAGNETZ_FILE ]; then
 
     # Adding the different magnet torrent
     for magnet in $(cat $MAGNETZ_FILE); do
-        transmission-remote --auth user:pwd -a "$magnet"
+        transmission-remote host:port --auth user:pwd -a "$magnet"
     done;
 
     rm $MAGNETZ_FILE
